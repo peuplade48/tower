c1f4e5609c29c3cecfaa4b64f1e8ec9d52334f6a
peuplade48 <saburhane92@gmail.com>
2026-03-22 21:03:39 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,8 @@
-pluginManagement {
-repositories {
-google()
-mavenCentral()
-gradlePluginPortal()
+pluginManagement {repositories {
+google {
+content {
+includeGroupByRegex("com\.android.")
+includeGroupByRegex("com\.google.")
+includeGroupByRegex("androidx.*")
 }
-}
-dependencyResolutionManagement {
-repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-repositories {
-google()
-mavenCentral()
-}
-}
-
-rootProject.name = "KingShotClone"
-include(":app")
+}mavenCentral()gradlePluginPortal()}}dependencyResolutionManagement {repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)repositories {google()mavenCentral()}}rootProject.name = "KingShotClone"include(":app")
